edf45143ea1b8a2ddbe17504ed1595d3bb55f8df
Eric De Wildt <eric.dewildt@publicmethod.net>
2019-02-13 21:26:56 -0500

    Finalize lesson flow and related code

--- app/src/main/java/com/examples/standardfunctions/Program.kt
+++ app/src/main/java/com/examples/standardfunctions/Program.kt
@@ -21,7 +21,7 @@ fun main() {
     // region Person
     data class UserId(val value: String = UUID.randomUUID().toString())
 
-    data class Person(
+    abstract class Person(
         val id: UserId,
         val name: String,
         val age: Int?,
@@ -29,12 +29,40 @@ fun main() {
         val friends: List<UserId> = emptyList()
     )
 
-    val bob = Person(
+    val bob = object : Person(
         UserId(),
         "Bob",
         25,
-        "Programmer"
-    )
+        "Kotlin Programmer"
+    ) {}
+
+    data class Purl(
+        private val knownProgrammingLanguages: List<String> = emptyList()
+    ) : Person(
+        id = UserId(),
+        name = "Purl",
+        age = 25,
+        profession = "Software Developer"
+    ) {
+        val clairPrinter = {
+            println("$name $age $profession $friends $knownProgrammingLanguages")
+        }
+    }
+
+    data class Lacy(
+        private val knownDesignPatterns: List<String> = emptyList(),
+        private val knownProgrammingLanguages: List<String> = emptyList()
+    ) : Person(
+        id = UserId(),
+        name = "Lacy",
+        age = 32,
+        profession = "Software Architect"
+    ) {
+        val lacyPrinter = {
+            println("$name $age $profession $friends $knownProgrammingLanguages $knownDesignPatterns")
+        }
+    }
+
     // endregion Person
 
     // region Person Functions
@@ -64,6 +92,7 @@ fun main() {
 
 
     // Different ways of calling functions
+    /*
     printDetailsFor(bob)
 
     bob.print()
@@ -75,79 +104,166 @@ fun main() {
     getPersonPrinter()(bob)
 
     getPersonPrinterExpression()(bob)
-
+*/
     // endregion Person Functions
 
-    /* with()
-    * Signature ==
-    */
-    fun <T, R> runCodeScopedTo(receiver: T, block: T.() -> R ): R = receiver.block()
+    // region run()
+    /*
+    * Java can run a block of code of arbitrary code like this:
+    *
+     {
+       someMethod()
+     }
+    *
+    * But Kotlin cant do that so we can use run() to execute a block of
+    * arbitrary code but with more semantic clarity.
+    * */
 
+    // Signature ==
+    fun <R> runArbitraryCode(block: () -> R): R = block()
 
-    // Different ways to call with()
-
-    with(bob) {
-        println("$name $age $profession $friends")
+    run {
+        println("Arbitrary Code Was Run")
     }
 
-    with(bob) {
-        printDetailsFor(this)
+    runArbitraryCode {
+        println("Arbitrary Code Was Run")
     }
 
-    with(bob) {
-        printExpression()
+    // Syntactically == too?
+    fun randomCode() {
+        println("Arbitrary Code Was Run")
     }
+    randomCode()
 
-    runCodeScopedTo(bob) {
-        print()
-    }
-
-    // Syntactically ~= too but using generics:
-    fun doStuffWithVariable(variable: Person) {
-        val name = variable.name
-        val age = variable.age
-        val profession = variable.profession
-        val friends = variable.friends
-
-        println("$name $age $profession $friends")
-    }
-
-    doStuffWithVariable(bob)
-
-    println("${bob.name} ${bob.age} ${bob.profession} ${bob.friends}")
 
     // Q: When do I use it?
-    // A: When accessing multiple properties / functions of an object
+    // A: When you just gotta run something like right there
 
-    // Q: When do I use with() instead of T.run() or T.apply()?
-    // A: It really depends but almost never
+    // Q: When do I use run() instead of T.run()?
+    // A: When you don't need T and you don't need a reference to the function
 
-    /* run()
+    // endregion run()
+
+
+    // region with()
+    // Signature ==
+    fun <T, R> runCodeScopedTo(receiver: T, block: T.() -> R): R = receiver.block()
+
+    // Different ways to call with()
+    /*
+    run {
+
+        with(bob) {
+            println("$name $age $profession $friends")
+        }
+
+        with(bob) {
+            printDetailsFor(this)
+        }
+
+        with(bob) {
+            printExpression()
+        }
+
+        with(bob, { print() })
+
+        runCodeScopedTo(bob) {
+            print()
+        }
+
+        // Syntactically ~= too but using generics:
+        fun doStuffWithVariable(variable: Person) {
+            val name = variable.name
+            val age = variable.age
+            val profession = variable.profession
+            val friends = variable.friends
+
+            println("$name $age $profession $friends")
+        }
+
+        doStuffWithVariable(bob)
+
+        println("${bob.name} ${bob.age} ${bob.profession} ${bob.friends}")
+
+        // Q: When do I use it?
+        // A: When accessing multiple properties / functions of an object
+
+        // Q: When do I use with() instead of T.run() or T.apply()?
+        // A: It really depends but almost never due to awkward null handling
+        //    and a more verbose syntax.
+    }
+    */
+    // endregion with()
+
+    // region T.run()
+
+    // Signature ==
+    fun <T, R> T.runArbitraryScopedCode(block: T.() -> R): R = block()
+
+    /*
     Syntactically == too?
     When to use it?
     When do I use it instead of ?
      */
+    // endregion T.run()
 
 
-    /* T.run()
-    Syntactically == too?
-    When to use it?
-    When do I use it instead of ?
-     */
-
-
+    // region T.let()
     /* T.let()
     Syntactically == too?
     When to use it?
     When do I use it instead of ?
      */
+    // endregion T.let()
 
 
+    // region T.apply()
     /* T.apply()
     Syntactically == too?
     When to use it?
     When do I use it instead of ?
      */
+    // endregion T.apply()
+
+
+    // region Putting It All Together
+
+    // Lacy builder DSL made using what we have learned
+    class LacyBuilderScope {
+        private val _programmingLanguages = mutableListOf<String>()
+        private val _designPatterns = mutableListOf<String>()
+
+        fun addProgrammingLanguage(
+            block: () -> String
+        ) = _programmingLanguages.add(block())
+
+
+        fun addDesignPattern(
+            block: () -> String
+        ) = _designPatterns.add(block())
+
+        fun build() = Lacy(_designPatterns, _programmingLanguages)
+
+    }
+
+    fun buildLacy(block: LacyBuilderScope.() -> Unit): Lacy =
+        LacyBuilderScope().run {
+            block()
+            return@run build()
+        }
+
+    buildLacy {
+        addDesignPattern {
+            "Builder Pattern"
+        }
+
+        addProgrammingLanguage {
+            "Kotlin"
+        }
+    }
+
+    // endregion Putting It All Together
 
 
 }
